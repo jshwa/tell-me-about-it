a45fa5c429b4e237df20e8f54edf8ef89c2bbca7
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-11 14:54:33 -0600

    app pulls token and login from query params in URL

--- tell-me-about-it-client/src/App.js
+++ tell-me-about-it-client/src/App.js
@@ -4,8 +4,22 @@ import Clock from './components/Clock';
 import Github from './containers/Github'
 
 class App extends Component {
-  render() {
-    return (
+	componentDidMount() {
+		const params = this.getQueryParams();
+		debugger
+	}
+
+	getQueryParams() {
+		const query = window.location.search.substring(1);
+		const pairs = query.split('&').map((str) => str.split('='));
+		return pairs.reduce((memo, pair) => {
+		  memo[pair[0]] = pair[1];
+		  return memo;
+		}, {});
+	 }
+
+	render() {
+      return (
       <div className="App">
         <Clock />
         <Github /> 
