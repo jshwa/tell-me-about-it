5a7fc95e0e713ab9afbcfa58c73b9b1f57c810af
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-11 15:36:45 -0600

    pass store to children with Provider, then connect app to store and dispatch userData

--- tell-me-about-it-client/src/App.js
+++ tell-me-about-it-client/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 import Clock from './components/Clock';
-import Github from './containers/Github'
+import Github from './containers/Github';
+import { loginUser } from './actions/Users';
 
 class App extends Component {
 	componentDidMount() {
 		const params = this.getQueryParams();
-		debugger
+		this.props.loginUser(params);
 	}
 
 	getQueryParams() {
 		const query = window.location.search.substring(1);
-		const pairs = query.split('&').map((str) => str.split('='));
-		return pairs.reduce((memo, pair) => {
+		const params = query.split('&').map((str) => str.split('='));
+		return params.reduce((memo, pair) => {
 		  memo[pair[0]] = pair[1];
 		  return memo;
 		}, {});
@@ -28,4 +30,11 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+	return ({
+		token: state.token,
+		login: state.login
+	})
+}
+
+export default connect(mapStateToProps, {loginUser})(App);
