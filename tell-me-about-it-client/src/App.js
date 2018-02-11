5e0a8c54b63229d88c43f8796f839aa0fe245301
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-10 19:31:38 -0600

    move authenticator and token handler to app/lib so they will be loaded with the rest of the files

--- tell-me-about-it-client/src/App.js
+++ tell-me-about-it-client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Clock from './components/Clock'
+import Clock from './components/Clock';
+import GithubLogin from './components/GithubLogin'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Clock />
+        <GithubLogin /> 
       </div>
     );
   }
