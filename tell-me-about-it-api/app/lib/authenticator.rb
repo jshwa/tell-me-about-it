fedc7e452484f64991f96d47f11d0eb9c8e546ff
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-10 19:32:10 -0600

    move authenticator and token handler to app/lib so they will be loaded with the rest of the files

---
+++ tell-me-about-it-api/app/lib/authenticator.rb
@@ -0,0 +1,39 @@
+class Authenticator
+    def initialize(connection = Faraday.new)
+        @connection = connection
+    end
+
+    def github(code)
+        access_token_resp = fetch_github_access_token(code)
+        access_token = access_token_resp['access_token']
+        user_info_resp = fetch_github_user_info(access_token)
+    
+        {
+        issuer: ENV['CLIENT_URL'],
+        login: user_info_resp['login'],
+        name: user_info_resp['name'],
+        avatar_url: user_info_resp['avatar_url'],
+        github_id: user_info_resp['id']
+        }
+    end
+
+    private
+
+    def fetch_github_access_token(code)
+        resp = @connection.post "https://github.com/login/oauth/access_token", {
+        code:          code,
+        client_id:     ENV['GITHUB_CLIENT_ID'],
+        client_secret: ENV['GITHUB_CLIENT_SECRET']
+        }
+        raise IOError, 'FETCH_ACCESS_TOKEN' unless resp.success?
+        URI.decode_www_form(resp.body).to_h
+    end
+
+    def fetch_github_user_info(access_token)
+        resp = @connection.get "https://api.github.com/user", {
+        access_token: access_token
+        }
+        raise IOError, 'FETCH_USER_INFO' unless resp.success?
+        JSON.parse(resp.body)
+    end
+end
