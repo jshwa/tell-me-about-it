13405a53b14e73ca1bb279a35eed60c3ccf43c8b
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-10 19:29:56 -0600

    add github_id and use it for locating user in db

--- tell-me-about-it-api/app/controllers/authentication_controller.rb
+++ tell-me-about-it-api/app/controllers/authentication_controller.rb
@@ -2,14 +2,16 @@ class AuthenticationController < ApplicationController
     def github
       authenticator = Authenticator.new
       user_info = authenticator.github(params[:code])
-  
+      
       login = user_info[:login]
       name = user_info[:name]
+      github_id = user_info[:github_id]
       avatar_url = user_info[:avatar_url]
-  
+      
       token = TokenHandler.encode(login)
-      User.where(login: login).first_or_create!(
+      User.where(github_id: github_id).first_or_create!(
         name: name,
+        login:login,
         avatar_url: avatar_url
       )
       redirect_to "#{issuer}?token=#{token}"
