e805d37c191699f6d6f0dbd59f7136cfc9f5e236
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-12 03:03:37 -0600

    fixed checking if logged in without a token

--- tell-me-about-it-api/app/controllers/application_controller.rb
+++ tell-me-about-it-api/app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::API
-    def current_user
-        token = params[:token]
-        payload = TokenHandler.decode(token)
-        @current_user ||= User.find_by_github_id(payload[0]['sub'])
+   def current_user
+      if params[:token]
+         token = params[:token]
+         payload = TokenHandler.decode(token)
+         @current_user ||= User.find_by_github_id(payload[0]['sub'])
+      else
+         @current_user = nil
       end
+   end
     
       def logged_in?
         current_user != nil
