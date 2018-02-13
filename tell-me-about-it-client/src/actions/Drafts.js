8d0852cc77bf7ce504238bfae7b23cac4ab82547
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-12 23:33:17 -0600

    add action for loading a saved draft into the editor from the db

--- tell-me-about-it-client/src/actions/Drafts.js
+++ tell-me-about-it-client/src/actions/Drafts.js
@@ -1,3 +1,5 @@
+import { convertFromRaw, EditorState } from 'draft-js';
+
 const Api_Url = 'http://localhost:3001/api'
 
 export const addDraftPosts = drafts => {
@@ -16,8 +18,17 @@ export const getDraftPosts = token => {
 }
 
 export const saveEditorState = editorState => {
+   debugger
    return {
       type: "UPDATE_EDITOR_STATE",
       editorState: editorState
+   }
+}
+
+export const loadDraftPost = rawData => {
+   const contentState = convertFromRaw(rawData);
+   return {
+      type: "UPDATE_EDITOR_STATE",
+      editorState: EditorState.createWithContent(contentState)
    }
 }
