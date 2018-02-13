e7b271c0e23a823606b4056c07bc20570314dd2a
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-12 23:34:37 -0600

    connected to redux store so on click titles can query database for the draft post and pass it to the load post action

--- tell-me-about-it-client/src/components/DraftPosts.js
+++ tell-me-about-it-client/src/components/DraftPosts.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import './Posts.css'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loadDraftPost} from '../actions/Drafts';
+import { convertFromRaw } from 'draft-js';
+import './Posts.css';
 
-const DraftPosts = ({ posts }) => (
-    <ul className="Github-posts">
-        {posts.map(post => <li key={post.id}>{post.title}</li>)}
-    </ul>
-)
+class DraftPosts extends Component {
+   getPost = (event) => {
+      const id = event.target.dataset.id
+      fetch(`http://localhost:3001/api/posts/${id}?token=${this.props.token}`)
+      .then(resp => resp.json())
+      .then(post => this.props.loadDraftPost(post.draft_json))
+   }
 
-export default DraftPosts
+   render() {
+      return (
+         <ul className="Github-posts">
+            {this.props.posts.map(post => <li key={post.id} data-id={post.id} onClick={this.getPost}>{post.title}</li>)}
+         </ul>
+      )
+   }
+}
+
+const mapStateToProps = state => {
+   return {
+      token: state.userData.token
+   }
+}
+
+export default connect(mapStateToProps, { loadDraftPost })(DraftPosts)
