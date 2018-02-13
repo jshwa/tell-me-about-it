1788c484c124d48bce85de6e8caf8e3208d5d927
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-12 21:14:21 -0600

    create and handle button actions for styles in text editor

---
+++ tell-me-about-it-client/src/components/StyleButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class StyleButton extends React.Component {
+   constructor() {
+     super();
+     this.onToggle = (e) => {
+       e.preventDefault();
+       this.props.onToggle(this.props.style);
+     };
+   }
+   render() {
+     let className = 'PostEditor-styleButton';
+     if (this.props.active) {
+       className += ' PostEditor-activeButton';
+     }
+     return (
+       <span className={className} onMouseDown={this.onToggle}>
+         {this.props.label}
+       </span>
+     );
+   }
+  }
+
+  export default StyleButton
