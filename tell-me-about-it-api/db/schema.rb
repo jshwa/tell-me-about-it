776911f12138fc4bf046809524fb6528f58b524e
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-14 15:21:14 -0600

    add migration for likes column

--- tell-me-about-it-api/db/schema.rb
+++ tell-me-about-it-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213033834) do
+ActiveRecord::Schema.define(version: 20180214211925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180213033834) do
     t.datetime "updated_at", null: false
     t.json "draft_json"
     t.boolean "draft", default: true
+    t.integer "likes"
   end
 
   create_table "users", force: :cascade do |t|
