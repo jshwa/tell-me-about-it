6b820a21206feb1e4897f29ba7213a5fc1368603
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-14 00:34:43 -0600

    refactored draft posts, moved getpost function to drafter, so component doesn't need to be connected to redux store

--- tell-me-about-it-client/src/components/DraftPosts.js
+++ tell-me-about-it-client/src/components/DraftPosts.js
@@ -1,31 +1,17 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { loadDraftPost, setCurrentDraft } from '../actions/Drafts';
 import './Posts.css';
+import { Link } from 'react-router-dom';
 
 class DraftPosts extends Component {
-   getPost = (event) => {
-      const id = event.target.dataset.id
-      this.props.setCurrentDraft({id: id, isSaved: true})
-      fetch(`http://localhost:3001/api/posts/${id}?token=${this.props.token}`)
-      .then(resp => resp.json())
-      .then(post => this.props.loadDraftPost(post.draft_json))
-   
-   }
-
    render() {
       return (
          <ul className="Github-posts">
-            {this.props.posts.map(post => <li key={post.id} data-id={post.id} onClick={this.getPost}>{post.title}</li>)}
+            {this.props.posts.map(post => <Link to={`/drafts/${post.id}`}>
+                  <li key={post.id} data-id={post.id}>{post.title}</li>
+               </Link>)}
          </ul>
       )
    }
 }
 
-const mapStateToProps = state => {
-   return {
-      token: state.userData.token
-   }
-}
-
-export default connect(mapStateToProps, { loadDraftPost, setCurrentDraft })(DraftPosts)
+export default DraftPosts
