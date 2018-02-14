51905d1e2231772d187903fafb9df9863ebd3fb6
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-14 01:28:07 -0600

    prevent prompt when displaying posts

--- tell-me-about-it-client/src/components/Prompt.js
+++ tell-me-about-it-client/src/components/Prompt.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const Prompt = () => {
-   if (window.location.pathname === "/drafts/new" || /\/drafts\/\d*/.test(window.location.pathname)) {
+   if (window.location.pathname === "/drafts/new" || 
+      /\/drafts\/\d*/.test(window.location.pathname) || 
+      /\/posts\/\d*/.test(window.location.pathname)) {
       return null;
    }
    
