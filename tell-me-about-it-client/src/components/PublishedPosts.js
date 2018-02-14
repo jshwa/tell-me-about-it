b84cf151c5b85438387e05d3d6a0322ba76f2e17
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-14 01:29:20 -0600

    add links and handling for displaying drafter for posts

--- tell-me-about-it-client/src/components/PublishedPosts.js
+++ tell-me-about-it-client/src/components/PublishedPosts.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PublishedPosts = ({ posts }) => (
     <ul className="Github-posts">
-        {posts.map((post, index) => <li key={index}>{post.split("\"")[1]}</li>)}
+        {posts.map((post, index) => <Link to={`/posts/${index}`}><li key={index}>{post.split("\"")[1]}</li></Link>)}
     </ul>
 )
 
--- tell-me-about-it-client/src/containers/Drafter.js
+++ tell-me-about-it-client/src/containers/Drafter.js
@@ -2,18 +2,25 @@ import React, {Component} from 'react';
 import PostEditor from '../components/PostEditor';
 import { Link } from 'react-router-dom';
 import { setCurrentDraft, loadDraftPost } from '../actions/Drafts';
+import { displayPublishedPost } from '../actions/Posts';
 import './Drafter.css';
 import isEmpty  from 'lodash/isEmpty';
 import { connect } from 'react-redux';
 
 class Drafter extends Component {
    componentDidMount() {
-      if ( !isEmpty(this.props.match.params) ) {
+      if ( !isEmpty(this.props.match.params) && this.props.match.path === "/drafts/:id") {
          const id = this.props.match.params.id;
          this.props.setCurrentDraft({id: id, isSaved: true})
          fetch(`http://localhost:3001/api/posts/${id}?token=${this.props.token}`)
          .then(resp => resp.json())
          .then(post => this.props.loadDraftPost(post.draft_json))
+      } 
+
+      if ( !isEmpty(this.props.match.params) && this.props.match.path === "/posts/:id") {
+         const id = this.props.match.params.id;
+         this.props.setCurrentDraft({id: id, isSaved: true});
+         this.props.displayPublishedPost(this.props.posts[id]);
       }
    }
 
@@ -29,7 +36,8 @@ class Drafter extends Component {
 }
 
 const mapStateToProps = state => ({
-   token: state.userData.token
+   token: state.userData.token, 
+   posts: state.posts.posts
 })
 
-export default connect(mapStateToProps, { setCurrentDraft, loadDraftPost })(Drafter)
+export default connect(mapStateToProps, { setCurrentDraft, loadDraftPost, displayPublishedPost })(Drafter)
