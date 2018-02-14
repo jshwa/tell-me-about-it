741875761aee1d53617ffb78e561d3eb67145cda
jshwa <32717724+jshwa@users.noreply.github.com>
2018-02-14 00:32:47 -0600

    added conditions to prevent Prompt from showing when drafter is open

--- tell-me-about-it-client/src/components/Prompt.js
+++ tell-me-about-it-client/src/components/Prompt.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const Prompt = () => {
-   if (window.location.pathname === "/drafter") {
+   if (window.location.pathname === "/drafts/new" || /\/drafts\/\d*/.test(window.location.pathname)) {
       return null;
    }
    
    return (
    <div className="Prompt">
       It's about time you started writing!
-      <Link to='/drafter'><span className="blinking-cursor">I</span></Link>
+      <Link to='/drafts/new'><span className="blinking-cursor">I</span></Link>
    </div>
 )}
